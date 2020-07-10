7821510058121d6b2b44457609ea87d6c3306ff2
doldsimo <simon.dold@hs-furtwangen.de>
2020-07-10 18:28:13 +0200

    fuege HTML-Consolen Layout hinzu

--- build/build.js
+++ build/build.js
@@ -1,3 +1,21 @@
 "use strict";
-console.log("funktioniert");
+var textAdventure;
+(function (textAdventure) {
+    checkUserInput();
+    function checkUserInput() {
+        let inputField = document.getElementById("inputField");
+        // tslint:disable-next-line: typedef
+        inputField.addEventListener("keyup", function (_event) {
+            if (_event.key === "Enter") {
+                printOutput(inputField.value);
+                inputField.value = "";
+            }
+        });
+    }
+    function printOutput(_theOutputString) {
+        let divConsole = document.getElementById("console");
+        divConsole.innerHTML += _theOutputString + "<br/>" + "<hr>"; // Fuegt Inhalt in den Div-Console Container ein
+        divConsole.scrollTop = divConsole.scrollHeight - divConsole.clientHeight; // Nach jeder neuen Consolen Ausgabe nach unten Scrollen
+    }
+})(textAdventure || (textAdventure = {}));
 //# sourceMappingURL=build.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log("funktioniert");
+namespace textAdventure {
+    checkUserInput();
+
+    function checkUserInput(): void {
+        let inputField: HTMLInputElement = document.getElementById("inputField") as HTMLInputElement;
+        // tslint:disable-next-line: typedef
+        inputField.addEventListener("keyup", function (_event) {
+            if (_event.key === "Enter") {
+                printOutput(inputField.value);
+                inputField.value = "";
+            }
+        });
+    }
+
+    function printOutput(_theOutputString: string): void {
+        let divConsole: HTMLDivElement = document.getElementById("console") as HTMLDivElement;
+        divConsole.innerHTML += _theOutputString + "<br/>" + "<hr>"; // Fuegt Inhalt in den Div-Console Container ein
+        divConsole.scrollTop = divConsole.scrollHeight - divConsole.clientHeight;   // Nach jeder neuen Consolen Ausgabe nach unten Scrollen
+    }
+} 
